2866543fe8ad2954b8e543c9b0191c65cf2c761b
rincyxinh <122154416+rincyxinh@users.noreply.github.com>
2023-01-07 19:15:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    introTitle: 'Hi my darling',
+    introDesc: `08/01/2023,
+    btnIntro: 'Tap',
+    title: 'Happy birthday 18th',
+    desc: 'Chào mừng người yêu của em bước vào độ tuổi trưởng thành',
+    btnYes: 'Lời chúc sinh nhật ở đây,
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
